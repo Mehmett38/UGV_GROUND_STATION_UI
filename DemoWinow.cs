4a20b3fdea113751aa79ff80b95ac8c96d13b728
Mehmet Dincer <dmehmett38@gmail.com>
2024-02-16 20:33:33 +0300

    V1.0.0 first commit

---
+++ DemoWinow.cs
@@ -0,0 +1,30 @@
+/*****************************************************************************/
+/* Project  : AvionicsInstrumentControlDemo                                  */
+/* File     : DemoWondow.cs                                                  */
+/* Version  : 1                                                              */
+/* Language : C#                                                             */
+/* Summary  : Start window of the project, use to test the instruments       */
+/* Creation : 30/06/2008                                                     */
+/* Autor    : Guillaume CHOUTEAU                                             */
+/* History  :                                                                */
+/*****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AvionicsInstrumentControlDemo
+{
+    public partial class DemoWinow : Form
+    {
+        public DemoWinow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
